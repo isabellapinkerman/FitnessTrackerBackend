30ec41d5be6ba8c41d72714c47bdbe6decebf245
Robby Bacus <robbybacus@gmail.com>
2022-11-07 11:47:34 -0500

    Making tables

--- db/users.js
+++ db/users.js
@@ -13,26 +13,66 @@ async function createUser({ username, password }) {
     VALUES ($1, $2)
     ON CONFLICT (username) DO NOTHING
     RETURNING *;
-    `, 
-    [username, password]);
+    `,
+      [username, password]
+    );
 
     return user;
   } catch (error) {
     throw error;
   }
-
 }
 
 async function getUser({ username, password }) {
-
+  try {
+    const {
+      rows: [user],
+    } = await client.query(
+      `SELECT username, password
+      FROM users
+      WHERE username=${username}, password=${password}`
+    );
+    return user;
+  } catch (error) {
+    throw error;
+  }
 }
 
 async function getUserById(userId) {
+  try {
+    const {
+      rows: [user],
+    } = await client.query(
+      `SELECT id, username
+      FROM users
+      WHERE id=${userId}`
+    );
 
+    if (!user) {
+      return null;
+    }
+
+    return user;
+  } catch (error) {
+    throw error;
+  }
 }
 
-async function getUserByUsername(userName) {
+async function getUserByUsername(username) {
+  try {
+    const {
+      rows: [user],
+    } = await client.query(
+      `SELECT *
+      FROM users
+      WHERE username=$1`,
+      [username]
+    );
 
+    return user;
+  } catch (error) {
+    throw error;
+  }
 }
 
 module.exports = {
@@ -40,4 +80,4 @@ module.exports = {
   getUser,
   getUserById,
   getUserByUsername,
-}
+};
